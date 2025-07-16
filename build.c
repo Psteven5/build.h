f5227f9c3b41cdd30f20b0ca44ec2c05966a57d5
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-07-17 00:26:56 +0200

    Create build.c

---
+++ build.c
@@ -0,0 +1,39 @@
+#define BUILD_IMPLEMENTATION
+#include "build.h"
+
+#ifdef _WIN32
+  #define CC "cl"
+#else
+  #define CC "cc"
+#endif
+
+int main(int argc, char *argv[])
+{
+  char *const result = strrchr(argv[0], '/');
+  if (NULL != result)
+    *result = '\0';
+  char const *const src_dir = argv[0];
+  
+  if (build_refresh("./build", (struct build_exe) {
+    .src_dir  = src_dir,
+    .deps     = BUILD_LIST(__FILE__),
+    .compiler = CC,
+    .srcs     = BUILD_LIST(__FILE__),
+  })) return EXIT_SUCCESS;
+
+  build_lib("./lib/hello.o", (struct build_lib) {
+    .src_dir  = src_dir,
+    .deps     = BUILD_LIST("./src/hello.c", "./build"),
+    .inc_dirs = BUILD_LIST("./include"),
+    .compiler = CC,
+    .srcs     = BUILD_LIST("./src/hello.c),
+  });
+  
+  build_exe("./bin/main", (struct build_exe) {
+    .src_dir  = src_dir,
+    .deps     = BUILD_LIST("./src/main.c", "./lib/hello.o"),
+    .compiler = CC,
+    .inc_dirs = BUILD_LIST("./include"),
+    .srcs     = BUILD_LIST("./src/main.c"),
+  });
+}
