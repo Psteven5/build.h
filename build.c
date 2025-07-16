9ca3347f394ed03371ffcda32a8889d6b9015428
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-07-17 00:46:23 +0200

    Update build.c

--- build.c
+++ build.c
@@ -34,6 +34,6 @@ int main(int argc, char *argv[])
                 .deps     = BUILD_LIST("./src/main.c", "./lib/hello.o"),
                 .compiler = CC,
                 .inc_dirs = BUILD_LIST("./include"),
-                .srcs     = BUILD_LIST("./src/main.c"),
+                .srcs     = BUILD_LIST("./lib/hello.o", "./src/main.c"),
         });
 }
