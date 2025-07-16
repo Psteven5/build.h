2cf973502009f5db804f9fab2c6e15a7e87df738
Pieter Stevens <122227977+Psteven5@users.noreply.github.com>
2025-07-17 01:26:53 +0200

    Update build.c

--- build.c
+++ build.c
@@ -15,28 +15,34 @@ int main(int argc, char *argv[])
                 *result = '\0';
                 src_dir = argv[0];
         }
-        
-        if (build_refresh("./build" BUILD_EXE, (struct build_exe) {
+
+        char const *const target_build =
+                "./build" BUILD_EXE;
+        if (build_refresh(target_build, (struct build_exe) {
                 .src_dir  = src_dir,
                 .deps     = BUILD_LIST(__FILE__),
                 .compiler = CC,
                 .srcs     = BUILD_LIST(__FILE__),
         })) return EXIT_SUCCESS;
-        
-        build_lib("./lib/hello" BUILD_SHARED, (struct build_lib) {
+
+        char const *const target_hello =
+                "./lib/hello" BUILD_SHARED;
+        build_lib(target_hello, (struct build_lib) {
                 .src_dir  = src_dir,
-                .deps     = BUILD_LIST("./src/hello.c", "./build" BUILD_EXE),
+                .deps     = BUILD_LIST(target_build, "./src/hello.c"),
                 .inc_dirs = BUILD_LIST("./include"),
                 .compiler = CC,
                 .srcs     = BUILD_LIST("./src/hello.c"),
         });
-        
-        build_exe("./bin/main" BUILD_EXE, (struct build_exe) {
+
+        char const *const target_main =
+                "./bin/main" BUILD_EXE;
+        build_exe(target_main, (struct build_exe) {
                 .src_dir  = src_dir,
-                .deps     = BUILD_LIST("./src/main.c", "./lib/hello" BUILD_SHARED),
+                .deps     = BUILD_LIST(target_hello, "./src/main.c"),
                 .compiler = CC,
                 .inc_dirs = BUILD_LIST("./include"),
                 .srcs     = BUILD_LIST("./src/main.c"),
-                .libs     = BUILD_LIST("./lib/hello" BUILD_SHARED),
+                .libs     = BUILD_LIST(target_hello),
         });
 }
